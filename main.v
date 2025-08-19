2ad5e876976a614593540c215ab733bb71114a2f
Tristan Henning <tristan@customcrypto.com>
2025-08-18 23:36:15 -0500

    started with login/admin workflow.

--- main.v
+++ main.v
@@ -13,6 +13,7 @@ pub mut:
 // App is shared by all requests. It manages the veb server in whole. 
 pub struct App {
 	veb.StaticHandler
+	veb.Middleware[Context]
 pub:
 	port		int
 	article_db	sqlite.DB
@@ -25,8 +26,15 @@ pub mut:
 // When deploying to prod: $ v -prod -o v_blogger .
 
 // TODOs:
-// 		1. Use the veb asset manager to eliminate the need for the StaticHandler https://modules.vlang.io/veb.assets.html
+// 		1. Use V's template engine to insert the css and js if performance with the static handler becomes a bottleneck
+// 			- How to measure tho?
 // 		2. Create Login, Sessions, auth validation middle ware
+// 			- Create and validate sessions
+//			- Set session cookie on long
+// 			- Read session from cookie on middleware hit and validate
+// 			- setup secure.db
+// 			- setup account registration
+// 			- setup login checking
 
 fn main() {
     mut app := &App{
@@ -41,6 +49,8 @@ fn main() {
     } or { panic(err) }
 
 	app.handle_static('static', true) or { panic(err) }
+
+	app.use(handler: app.check_login)
     
     veb.run[App, Context](mut app, app.port)
 }
@@ -60,6 +70,14 @@ pub mut:
 	summary		string
 }
 
+// ----------------
+// -- Middleware --
+// ----------------
+pub fn (app &App) check_login (mut ctx Context) bool {
+    ctx.is_admin = true
+	return true
+}
+
 // -------------------
 // -- Public Routes -- 
 // -------------------
@@ -70,42 +88,84 @@ pub fn (app &App) index(mut ctx Context) veb.Result {
 	return $veb.html()
 }
 
+@[post]
+pub fn (app &App) login(mut ctx Context) veb.Result {
+	username := ctx.form['username']
+	password := ctx.form['password']
+	if (username == 'tristan') && (password == 'test'){
+		return ctx.redirect('/admin', typ: .see_other)
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
+}
+
 // --------------------
 // -- Private Routes --
 // --------------------
 
 pub fn (app &App) admin(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
 
 pub fn (app &App) export(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
 
 pub fn (app &App) import(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
 
 pub fn (app &App) manageadmins(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
 
 pub fn (app &App) manageposts(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
 
 pub fn (app &App) newpost(mut ctx Context) veb.Result {
-	title := app.title
-	tab_title := app.tab_title
-	return $veb.html()
+	if ctx.is_admin {
+		title := app.title
+		tab_title := app.tab_title
+		return $veb.html()
+	}
+	else {
+		return ctx.redirect('/', typ: .see_other)
+	}
 }
